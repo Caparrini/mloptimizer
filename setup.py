f0f8946151b0d6cce90c3d5c1acd8f2b092c0466
caparrini <a.caparrini@icloud.com>
2021-07-11 16:30:26 +0200

    FIX: Errors due to changes in pip

--- setup.py
+++ setup.py
@@ -1,20 +1,35 @@
 import setuptools
-from pip._internal.req import parse_requirements
-
+import os
+try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
+    from pip._internal.req import parse_requirements
+except ImportError:
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
+print(os.listdir())
+print(os.getcwd())
+import pathlib
+print(pathlib.Path(__file__).parent.resolve())
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt", session=False)
+install_reqs = parse_requirements("/Users/paradox/workspace/mloptimizer/requirements.txt", session=False)
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
+reqs = [str(ir.requirement) for ir in install_reqs]
 
 setuptools.setup(
     name='mloptimizer',
-    version='0.4',
+    version='0.5',
     author="Antonio Caparrini",
     author_email="a.caparrini@gmail.com",
     description="Genetic hyper-parameter selection for machine learning algorithms",
