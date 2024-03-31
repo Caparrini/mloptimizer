c877b63852bc7555eb006c2bcb729a27aceff2f1
Caparrini <a.caparrini@icloud.com>
2024-03-31 22:52:30 +0200

    Mod: version 0.7.1

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'mloptimizer'
 copyright = '2024, Antonio Caparrini'
 author = 'Antonio Caparrini'
-release = '0.7.0'
+release = '0.7.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
